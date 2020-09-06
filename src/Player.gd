4b34ae2c22eeeac1507ebecd162f41ed514bba7a
Andrew <astafford2@bsu.edu>
2020-09-06 01:32:37 -0400

    Set changing of trajectory line angle by radians and clamp trajectory

--- src/Player.gd
+++ src/Player.gd
@@ -4,15 +4,22 @@ export var strength : float = 300
 var trajPoint = Vector2(100, -100)
 
 func _process(delta):
-	$Line2D.set_point_position(1, trajPoint)
+	$Trajectory.set_point_position(1, trajPoint)
+	trajPoint = Vector2(clamp(trajPoint.x, 0, 1090), clamp(trajPoint.y, -500, 0))
 	
 	if Input.is_action_just_pressed("launch"):
 		var direction = trajPoint.normalized()
 		var velocity = direction * strength
 		apply_impulse(Vector2.ZERO, velocity)
-	if Input.is_action_just_pressed("ui_right"):
-		trajPoint.x += 10
-		trajPoint.y += 10
-	if Input.is_action_just_pressed("ui_left"):
-		trajPoint.x -= 10
-		trajPoint.y -= 10
+		$Trajectory.visible = false
+	
+	if Input.is_action_pressed("ui_right"):
+		trajPoint = trajPoint.rotated(PI/180)
+	if Input.is_action_pressed("ui_left"):
+		trajPoint = trajPoint.rotated(-PI/180)
+	if Input.is_action_just_pressed("ui_up"):
+		$Trajectory.width += 2
+		strength += 50
+	if Input.is_action_just_pressed("ui_down"):
+		$Trajectory.width -= 2
+		strength -= 50
