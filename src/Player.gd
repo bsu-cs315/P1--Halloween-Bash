069d7287faad6faeabfd66c88430783bc108636a
Andrew <astafford2@bsu.edu>
2020-09-05 23:28:38 -0400

    Instance tile ground and player sprite into level scene

--- src/Player.gd
+++ src/Player.gd
@@ -8,7 +8,11 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# gravity_scale = 0
+	pass
+	
+func _integrate_forces(state):
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
