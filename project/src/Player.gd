65119e3af5b3f8299a1381052e501378419fa8cb
Andrew <astafford2@bsu.edu>
2020-09-10 23:30:32 -0400

    Use range_lerp versus clamp for power length

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -1,10 +1,16 @@
 extends RigidBody2D
 
-export var strength : float = 300
 var _angle : float = 0
 var _angleIncrement : float = PI/180
-var powerLength : float = 100
+var _power : float = 300
+var _powerLength : float = 100
+var _powerLengthIncrement : float = 5
 var _isFired : bool = false
+
+const MIN_POWER : float = 300.0
+const MAX_POWER : float = 800.0
+const MIN_POWER_LENGTH : float = 100.0
+const MAX_POWER_LENGTH : float = 200.0
 
 func _ready():
 	gravity_scale = 0
@@ -14,12 +20,12 @@ func _process(delta):
 		return
 	
 	_angle = clamp(_angle, -PI/2, 0)
-	strength = clamp(strength, 300, 800)
-	powerLength = clamp(powerLength, 100, 200)
+	_power = clamp(_power, MIN_POWER, MAX_POWER)
+	_powerLength = range_lerp(_power, MIN_POWER, MAX_POWER, MIN_POWER_LENGTH, MAX_POWER_LENGTH)
 	
 	if Input.is_action_just_pressed("launch"):
 		var direction = Vector2(1, 0).rotated(_angle)
-		var velocity = direction * strength
+		var velocity = direction * _power
 		apply_impulse(Vector2.ZERO, velocity)
 		gravity_scale = 3
 		_isFired = true
@@ -28,15 +34,15 @@ func _process(delta):
 		_angle += _angleIncrement
 	if Input.is_action_pressed("ui_left"):
 		_angle -= _angleIncrement
-	if Input.is_action_just_pressed("ui_up"):
-		powerLength += 10
-		strength += 50
-	if Input.is_action_just_pressed("ui_down"):
-		powerLength -= 10
-		strength -= 50
+	if Input.is_action_pressed("ui_up"):
+		_powerLength += _powerLengthIncrement
+		_power += 25
+	if Input.is_action_pressed("ui_down"):
+		_powerLength -= _powerLengthIncrement
+		_power -= 25
 		
 	update()
 	
 func _draw():
 	if not _isFired:
-		draw_line(Vector2.ZERO, Vector2(powerLength, 0).rotated(_angle), Color.gray, 2)
+		draw_line(Vector2.ZERO, Vector2(_powerLength, 0).rotated(_angle), Color.gray, 2)
