01d03262dd4a96fa4375f534e89d42c9e48f8f37
Andrew <astafford2@bsu.edu>
2020-09-13 14:37:47 -0400

    Add edge limits to world
    boxes falling lower box count
    player falling respawns new player projectile

--- project/src/Level.gd
+++ project/src/Level.gd
@@ -16,20 +16,10 @@ func _process(delta):
 	$ProjectileCounter/Background/Number.text = str(_projectileCount)
 	$BoxCounter/Background/Number.text = str(_boxCount)
 	if _projectileCount > 0 and player.sleeping:
-		_on_Player_sleeping_state_changed()
-	
-	
-func update_score():
-	$ScoreLabel.text = "Target Hit!"
+		_on_Player_stopped()
 
 
-func _on_Player_body_shape_entered(body_id, body, body_shape, local_shape):
-	if body_id == $Target.get_instance_id() or \
-	body_id == $Target2.get_instance_id():
-		update_score()
-
-
-func _on_Player_sleeping_state_changed():
+func _on_Player_stopped():
 	player.queue_free()
 	_projectileCount -= 1
 	
@@ -37,3 +27,18 @@ func _on_Player_sleeping_state_changed():
 		player = Player.instance()
 		add_child(player)
 		player.position = $StartPosition.position
+
+
+func _on_EdgeLimit_body_shape_entered(body_id, body, body_shape, area_shape):
+	if $Target != null:
+		if body_id == $Target.get_instance_id():
+			$Target.queue_free()
+			_boxCount -= 1
+	if $Target2 != null:
+		if body_id == $Target2.get_instance_id():
+			$Target2.queue_free()
+			_boxCount -= 1
+	if player != null:
+		if body_id == player.get_instance_id():
+			_on_Player_stopped()
+		
